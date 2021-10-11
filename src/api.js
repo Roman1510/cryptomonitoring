5506059d76c4e5a8dc01f6e49b79aea3f88e819e
Roman1510 <romanvinnick@gmail.com>
2021-10-11 17:31:36 +0200

    fix for multiple tabs

--- src/api.js
+++ src/api.js
@@ -1,39 +1,40 @@
 const api_key = process.env.VUE_APP_API_KEY; //api key
 const currenciesHandlers = new Map();
 const AGGREGATE_INDEX = "5";
-const socket = new WebSocket(`wss://streamer.cryptocompare.com/v2?api_key=${api_key}`);
-const bc = new BroadcastChannel('my-broadcast')
-bc.postMessage('close')
-bc.addEventListener('message',e=>{
-  if(e.data==='close'){
-    socket.close();
+
+const bc = new BroadcastChannel('wsdata')
+bc.onmessage = ev => {
+  if(ev.data){
+    const handlers = currenciesHandlers.get(ev.data.currency) ?? [];
+    if (ev.data.newPrice) {
+      handlers.forEach(fn => fn(ev.data.newPrice));
+    }
   }
-})
-//i have to wrap the whole ws messaging inside a condition where i see if there's a message already with the content ('data', etc...)
-//but before this I have to send a message that I opened a new tab, and when the old tab hears for the message,
-// it sends back the data from the WS, where I just refresh that
-//1) the multiple tabs "MESSAGE": "TOO_MANY_SOCKETS_MAX_1_PER_CLIENT",
-//2) graph fix
-socket.addEventListener("message", (e) => {
+}
+
+const socket =  new WebSocket(`wss://streamer.cryptocompare.com/v2?api_key=${api_key}`);
+socket?.addEventListener("message", (e) => {
   const { TYPE: type, FROMSYMBOL: currency, PRICE: newPrice } = JSON.parse(e.data);
   if (type !== AGGREGATE_INDEX) {
     return;
+  }
+  if(currency&&newPrice){
+    bc.postMessage({'type':type,'currency':currency,'newPrice':newPrice})
   }
   const handlers = currenciesHandlers.get(currency) ?? [];
   if (newPrice) {
     handlers.forEach(fn => fn(newPrice));
   }
-
 });
 
 function sendToWS(message) {
   const stringMessage = JSON.stringify(message);
-  if (socket.readyState === WebSocket.OPEN) {
-    socket.send(stringMessage);
+  if (socket?.readyState === WebSocket.OPEN) {
+    socket?.send(stringMessage);
     return;
   }
-  socket.addEventListener("open", () => {
-    socket.send(stringMessage);
+  socket?.addEventListener("open", () => {
+    socket?.send(stringMessage);
   }, { once: true });
 }
 
@@ -61,3 +62,7 @@ export const unsubscribeFromCurrency = currency => {
   currenciesHandlers.delete(currency);
   unsubscribeOnWS(currency);
 };
+
+
+//1) the multiple tabs "MESSAGE": "TOO_MANY_SOCKETS_MAX_1_PER_CLIENT",
+//2) graph fix
